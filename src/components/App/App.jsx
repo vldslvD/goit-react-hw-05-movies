4ceb7445189267122c98bb9bacb1a38c170f9a7c
vldslvD <dobritsavlad@gmail.com>
2023-09-10 08:49:44 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,20 +4,22 @@ import Layout from 'components/Layout/Layout';
 const Cast = lazy(() => import('../Cast/Cast'));
 const Reviews = lazy(() => import('../Reviews'));
 const Home = lazy(() => import('../../pages/Home'));
-const MovieDetails = lazy(() => import('../../pages/MovieDetails/MovieDetails'));
+const MovieDetails = lazy(() =>
+  import('../../pages/MovieDetails/MovieDetails')
+);
 const SearchMovie = lazy(() => import('../../pages/SearchMovie'));
 
 export const App = () => {
   return (
-      <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route index element={<Home />}></Route>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />}></Route>
         <Route path="movies" element={<SearchMovie />}></Route>
         <Route path="movies/:movieId" element={<MovieDetails />}>
-          <Route path="cast" element={<Cast/>}></Route>
-          <Route path="reviews" element={<Reviews/>}></Route>
+          <Route path="cast" element={<Cast />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
         </Route>
-        </Route>
-      </Routes>
+      </Route>
+    </Routes>
   );
 };
